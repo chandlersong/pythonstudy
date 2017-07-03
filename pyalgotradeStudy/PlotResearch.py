d171b6b20eb41eb7c91456bdd7b19b0e022f8058
chandler.song <Song.Qingxin@flatironssolutions.com>
2017-07-03 10:54:06 +0800

    pyalgotradeStudy:refactory stragegy

--- pyalgotradeStudy/PlotResearch.py
+++ pyalgotradeStudy/PlotResearch.py
@@ -1,51 +1,8 @@
 from pyalgotrade import strategy, plotter
 from pyalgotrade.barfeed import googlefeed
 from pyalgotrade.stratanalyzer import returns
-from pyalgotrade.technical import ma, cross
+from pyalgotradeStudy.Strategy import SMACrossOver
 
-
-class SMACrossOver(strategy.BacktestingStrategy):
-    def __init__(self, feed, instrument, smaPeriod):
-        super(SMACrossOver, self).__init__(feed)
-        self.__instrument = instrument
-        self.__position = None
-        # We'll use adjusted close values instead of regular close values.
-        self.__prices = feed[instrument].getPriceDataSeries()
-        self.__sma = ma.SMA(self.__prices, smaPeriod)
-
-    def getSMA(self):
-        return self.__sma
-
-    def onEnterCanceled(self, position):
-        self.__position = None
-
-    def onExitOk(self, position):
-        self.__position = None
-
-    def onExitCanceled(self, position):
-        # If the exit was canceled, re-submit it.
-        self.__position.exitMarket()
-
-    def onBars(self, bars):
-        bar = bars[self.__instrument]
-        # If a position was not opened, check if we should enter a long position.
-        if self.__position is None:
-            if cross.cross_above(self.__prices, self.__sma) > 0:
-                shares = int(self.getBroker().getCash() * 0.9 / bars[self.__instrument].getPrice())
-                # Enter a buy market order. The order is good till canceled.
-                self.__position = self.enterLong(self.__instrument, shares, True)
-        # Check if we have to exit the position.
-        elif not self.__position.exitActive() and cross.cross_below(self.__prices, self.__sma) > 0:
-            self.__position.exitMarket()
-
-        if cross.cross_below(self.__prices, self.__sma):
-            print("crocess below,prices %s, sma %s" % (bar.getClose(),self.__sma[-1]))
-            print("crocess below,prices %s, sma %s" % (self.__prices[-1],self.__sma[-1]))
-        elif cross.cross_above(self.__prices, self.__sma):
-            print("crocess above,prices %s, sma %s" % (bar.getClose(), self.__sma[-1]))
-
-
-# Load the yahoo feed from the CSV file
 feed = googlefeed.Feed()
 feed.addBarsFromCSV("orcl", "orcl-2000.csv")
 
---
+++ pyalgotradeStudy/Strategy.py
@@ -0,0 +1,82 @@
+from pyalgotrade import strategy
+from pyalgotrade.technical import ma, cross
+
+
+class SimpleStrategy(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, smaPeriod):
+        super(SimpleStrategy, self).__init__(feed, 1000)
+        self.__position = None
+        self.__instrument = instrument
+        # We'll use adjusted close values instead of regular close values.
+        self.__sma = ma.SMA(feed[instrument].getPriceDataSeries(), smaPeriod)
+
+    def onEnterOk(self, position):
+        execInfo = position.getEntryOrder().getExecutionInfo()
+        self.info("BUY at $%.2f" % (execInfo.getPrice()))
+
+    def onEnterCanceled(self, position):
+        self.__position = None
+
+    def onExitOk(self, position):
+        execInfo = position.getExitOrder().getExecutionInfo()
+        self.info("SELL at $%.2f" % (execInfo.getPrice()))
+        self.__position = None
+
+    def onExitCanceled(self, position):
+        # If the exit was canceled, re-submit it.
+        self.__position.exitMarket()
+
+    def onBars(self, bars):
+        # Wait for enough bars to be available to calculate a SMA.
+        if self.__sma[-1] is None:
+            return
+
+        bar = bars[self.__instrument]
+        # If a position was not opened, check if we should enter a long position.
+        if self.__position is None:
+            if bar.getPrice() > self.__sma[-1]:
+                # Enter a buy market order for 10 shares. The order is good till canceled.
+                self.__position = self.enterLong(self.__instrument, 10, True)
+        # Check if we have to exit the position.
+        elif bar.getPrice() < self.__sma[-1] and not self.__position.exitActive():
+            self.__position.exitMarket()
+
+class SMACrossOver(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, smaPeriod):
+        super(SMACrossOver, self).__init__(feed)
+        self.__instrument = instrument
+        self.__position = None
+        # We'll use adjusted close values instead of regular close values.
+        self.__prices = feed[instrument].getPriceDataSeries()
+        self.__sma = ma.SMA(self.__prices, smaPeriod)
+
+    def getSMA(self):
+        return self.__sma
+
+    def onEnterCanceled(self, position):
+        self.__position = None
+
+    def onExitOk(self, position):
+        self.__position = None
+
+    def onExitCanceled(self, position):
+        # If the exit was canceled, re-submit it.
+        self.__position.exitMarket()
+
+    def onBars(self, bars):
+        bar = bars[self.__instrument]
+        # If a position was not opened, check if we should enter a long position.
+        if self.__position is None:
+            if cross.cross_above(self.__prices, self.__sma) > 0:
+                shares = int(self.getBroker().getCash() * 0.9 / bars[self.__instrument].getPrice())
+                # Enter a buy market order. The order is good till canceled.
+                self.__position = self.enterLong(self.__instrument, shares, True)
+        # Check if we have to exit the position.
+        elif not self.__position.exitActive() and cross.cross_below(self.__prices, self.__sma) > 0:
+            self.__position.exitMarket()
+
+        if cross.cross_below(self.__prices, self.__sma):
+            print("crocess below,prices %s, sma %s" % (bar.getClose(),self.__sma[-1]))
+            print("crocess below,prices %s, sma %s" % (self.__prices[-1],self.__sma[-1]))
+        elif cross.cross_above(self.__prices, self.__sma):
+            print("crocess above,prices %s, sma %s" % (bar.getClose(), self.__sma[-1]))
