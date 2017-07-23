331dafa8709ba7e963479644d3ae25f8bd5cd76d
chandlersong <chandler605@gmail.com>
2017-07-23 22:58:18 +0800

    pyalgotradeStudy.feed:study frequency

--- pyalgotradeStudy/customFeed.py
+++ pyalgotradeStudy/customFeed.py
@@ -2,12 +2,11 @@ import datetime
 import unittest
 from unittest import TestCase
 
+import pandas as pd
 from pyalgotrade import bar, strategy
 from pyalgotrade.bar import BasicBar
 from pyalgotrade.barfeed import membf
 from pyalgotrade.technical import rsi, ma
-
-import pandas as pd
 
 
 class MyStrategy(strategy.BacktestingStrategy):
@@ -51,21 +50,59 @@ class MyFeed(membf.BarFeed):
 
     def _parseDate(self, dateString):
         ret = datetime.datetime.strptime(dateString, self.__dateTimeFormat)
-
-
         return ret
+
+
+class FiveMinsFeed(membf.BarFeed):
+    def __init__(self, frequency=bar.Frequency.MINUTE * 5, timezone=None, maxLen=None):
+        super(FiveMinsFeed, self).__init__(frequency, maxLen)
+
+        self.__timezone = timezone
+        self.__sanitizeBars = False
+        self.__dateTimeFormat = "%Y-%m-%d %H:%M:%S"
+
+    def readFromPandas(self, instrument, df, timezone=None):
+        loadedBars = []
+        for index, row in df.iterrows():
+            dateTime = index
+            open_ = float(row[3])
+            high = float(row[1])
+            low = float(row[2])
+            close = float(row[0])
+            volume = float(row[4])
+            bar_ = BasicBar(
+                dateTime, open_, high, low, close, volume, None, bar.Frequency.DAY, extra={})
+
+            loadedBars.append(bar_)
+
+        self.addBarsFromSequence(instrument, loadedBars)
 
 
 class TestCustomFeed(TestCase):
     def setUp(self):
         self.data = pd.read_csv("orcl-2000.csv", index_col="Date")
-        #print(self.data)
+        # print(self.data)
 
     def test_helloworld(self):
         feed = MyFeed()
         feed.readFromPandas("orcl", self.data)
 
         # Evaluate the strategy with the feed's bars.
+        myStrategy = MyStrategy(feed, "orcl")
+        myStrategy.run()
+
+
+class TestFiveMinsFeed(TestCase):
+    def setUp(self):
+        stock_prices = pd.read_csv("five_mintus.csv")
+        stock_prices.columns = ['date', 'open', 'high', 'low', 'close', 'volume', 'delete']
+        stock_prices = stock_prices.drop(['delete'], axis=1)
+        stock_prices['date'] = pd.to_datetime(stock_prices['date'])
+        self.data = stock_prices.set_index(['date']).sort_index(axis=1)
+
+    def test_helloworld(self):
+        feed = FiveMinsFeed()
+        feed.readFromPandas("orcl", self.data)
         myStrategy = MyStrategy(feed, "orcl")
         myStrategy.run()
 
