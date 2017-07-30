b5e25406a936b5c57c5c9eefe0dcff79d1ff398e
chandlersong <chandler605@gmail.com>
2017-07-30 14:44:12 +0800

    intersection：add example of intersection

--- basic/dictStudy.py
+++ basic/dictStudy.py
@@ -1,12 +1,22 @@
 import unittest
 from unittest import TestCase
 
+
 class DictTest(TestCase):
     def test_create_dict(self):
         rec = {'name': {'first': 'Bob', 'last': 'Smith'},
                'jobs': ['dev', 'mgr'],
                'age': 40.5}
         print(rec)
+
+    def test_intersection(self):
+        X = set('spam')
+        Y = {'h', 'a', 'm'}
+
+        print("Intersection"+str(X & Y))
+        print("Union"+str(X | Y))
+        print("Difference" + str(X - Y))
+        print("Superset:" + str( X > Y)) #超集
 
 if __name__ == '__main__':
     unittest.main()
