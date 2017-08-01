546907b5a5c14e3308da17f624b7ea10256e3921
chandlersong <chandler605@gmail.com>
2017-08-01 22:57:37 +0800

    pandas.DataFrame:add an example about how to add column

--- pandasStudy/DataFrameSelectAndLoop.py
+++ pandasStudy/DataFrameSelectAndLoop.py
@@ -14,18 +14,24 @@ class TestDataFrameExample(TestCase):
         print(self.example)
         print("========================================================================")
 
-    def loop_1(self):
+    def test_loop_1(self):
         data =  self.example.to_records()
         record = data[0]
         print(type(record))
         print(record.A)
 
-    def loop_2(self):
+    def test_loop_2(self):
         for index,row in self.example.iterrows():
             print(index)
             print(type(index))
             print(row)
             print(type(row))
+
+    def test_add_column(self):
+        self.example['new column'] = range(5)
+        print(self.example)
+
+
 
 if __name__ == '__main__':
     unittest.main()
