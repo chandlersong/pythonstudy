bb823426e308388e3d092b13a2883bd369102ac9
chandler.song <Song.Qingxin@flatironssolutions.com>
2017-08-01 14:24:08 +0800

    python:add some example of basic type

--- basic/dictStudy.py
+++ basic/dictStudy.py
@@ -9,6 +9,13 @@ class DictTest(TestCase):
                'age': 40.5}
         print(rec)
 
+    def test_sort_map(self):
+        D = {'a': 1, 'c': 3, 'b': 2}
+        for key in sorted(D):
+            print(key, '=>', D[key])
+
+
+
     def test_intersection(self):
         X = set('spam')
         Y = {'h', 'a', 'm'}
