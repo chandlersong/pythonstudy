a05f4be360836165e76fc0741fd91edc69c79fac
chandler.song <Song.Qingxin@flatironssolutions.com>
2017-08-09 17:00:46 +0800

    numeric:add some example

--- basic/numeric.py
+++ basic/numeric.py
@@ -1,6 +1,8 @@
 import unittest
 from unittest import TestCase
 
+import decimal
+from decimal import Decimal
 
 class TestBasicFeature(TestCase):
     def setUp(self):
@@ -25,8 +27,31 @@ class TestBasicFeature(TestCase):
     def test_division(self):
         X = 5
         Y = 2
-        print("X/Y:{0}".format(X / Y))    # 真实数据
+        print("X/Y:{0}".format(X / Y))  # 真实数据
         print("X//Y:{0}".format(X // Y))  # 求倍数
+
+        print("6/float(2):{0}".format(6 / float(2)))
+
+    def test_complex_number(self):
+        print("1j*1J:{0}".format(1j * 1J))
+
+    def test_hex_otc_bin(self):
+        number = 64
+        print("hex:{0},otc:{1},bin:{0},".format(oct(number),hex(number),bin(number)))
+
+
+class DecimalTest(TestCase):
+
+    def test_basic_feature(self):
+        print(Decimal('0.1') + Decimal('0.1') + Decimal('0.1') - Decimal('0.3'))
+
+    def test_set_global_precision(self):
+        print("before set precision")
+        print(decimal.Decimal(1) / decimal.Decimal(7))
+        decimal.getcontext().prec = 5
+        print("after set precision")
+        print(decimal.Decimal(1) / decimal.Decimal(7))
+
 
 
 if __name__ == '__main__':
