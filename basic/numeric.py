23413c3f2d7314e17c2de2150588948f8ae710e4
chandler.song <Song.Qingxin@flatironssolutions.com>
2017-08-09 10:32:40 +0800

    numeric:add division example

--- basic/numeric.py
+++ basic/numeric.py
@@ -1,26 +1,32 @@
 import unittest
 from unittest import TestCase
 
-class TestBasicFeature(TestCase):
 
+class TestBasicFeature(TestCase):
     def setUp(self):
         print()
 
     def test_mixd_type(self):
-        int_double = 40+3.14
+        int_double = 40 + 3.14
 
-        print("int + double:{0},type:{1}".format(int_double,type(int_double)))
+        print("int + double:{0},type:{1}".format(int_double, type(int_double)))
         print("the int will be promote to double.")
 
     def test_format(self):
-        num = 1/3.0
+        num = 1 / 3.0
         print(num)
-        print("%e"%num)
-        print('%4.2f'%num)
+        print("%e" % num)
+        print('%4.2f' % num)
         print('{0:4.2f}'.format(num))
 
     def test_chain_compara(self):
-        print(3>2>1) # equal to 3 > 2 and 2 >2 . but I don't suggest to use it
+        print(3 > 2 > 1)  # equal to 3 > 2 and 2 >2 . but I don't suggest to use it
+
+    def test_division(self):
+        X = 5
+        Y = 2
+        print("X/Y:{0}".format(X / Y))    # 真实数据
+        print("X//Y:{0}".format(X // Y))  # 求倍数
 
 
 if __name__ == '__main__':
