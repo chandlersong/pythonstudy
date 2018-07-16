d7774f9c810a57a28e38064afb98625df74c3227
chandlersong <chandler605@gmail.com>
2018-07-16 23:02:27 +0800

    add example of pandas compare

--- pandasStudy/DataFrameSelectAndLoop.py
+++ pandasStudy/DataFrameSelectAndLoop.py
@@ -19,6 +19,16 @@ class TestDataFrameExample(TestCase):
         print(self.example)
         print("========================================================================")
 
+    def test_empty(self):
+
+        empty = DataFrame()
+
+        if   empty.empty:
+            print("empty \n")
+        else:
+            print("not empty \n")
+
+
     def test_loop_1(self):
         data = self.example.to_records()
         record = data[0]
@@ -73,6 +83,7 @@ class TestDataFrameExample(TestCase):
 
         with request.urlopen('http://quotes.money.163.com/service/zcfzb_600073.html') as web:
             local = pd.read_csv(web, encoding='gb2312', na_values='--', index_col=index_col)
+            print(local.shape)
             result = local.drop(local.columns[len(local.columns) - 1], axis=1).fillna(0).apply(pd.to_numeric,
                                                                                                errors='coerce')
 
@@ -81,7 +92,7 @@ class TestDataFrameExample(TestCase):
             if os.path.exists(testfileName):
                 shutil.rmtree(testfileName)
             os.mkdir(testfileName)
-            testfileName = testfileName + "\\a.gzip"
+            testfileName = testfileName + "\\a.csv"
             print(testfileName)
             result.to_csv(testfileName, encoding='utf-8')
 
@@ -89,9 +100,19 @@ class TestDataFrameExample(TestCase):
             print(actual[latest_report])
             print(result[latest_report])
 
+            print(result.shape)
+            print(actual.shape)
+
+            print(result.columns)
+
             assert_series_equal(result[latest_report], actual[latest_report], check_dtype=False)
             assert_frame_equal(result, actual)
 
+        with request.urlopen('http://quotes.money.163.com/service/zcfzb_600096.html') as web:
+            origin = pd.read_csv(web,  encoding='gb2312')
+            origin.to_csv(tempfile.gettempdir() + "\\test\\600096.csv")
+
+        print(tempfile.gettempdir() + "\\test")
 
 if __name__ == '__main__':
     unittest.main()
