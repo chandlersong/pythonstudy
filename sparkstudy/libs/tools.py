fb55e70479251705d08b75ea5a0c65705279e04b
chandlersong <chandler605@gmail.com>
2020-09-03 22:19:46 +0800

    hello world: project code will automatically push to spark

--- sparkstudy/libs/tools.py
+++ sparkstudy/libs/tools.py
@@ -1,5 +1,5 @@
 from pyspark.sql import DataFrame
 
 
-def test_app(log_data: DataFramke, check_char: str = 'a'):
+def test_app(log_data: DataFrame, check_char: str = 'a'):
     return log_data["age"].contains(check_char)
