54e6fb194c8aaaa09608aab4c9a76f05f259c978
chandler <chandler605@gmail.com>
2023-01-17 12:40:35 +0800

    add run multi example

--- basic/Bbands.py
+++ basic/Bbands.py
@@ -1,4 +1,5 @@
 from backtrader import Strategy
+from loguru import logger
 
 
 class BbandsStrategy(Strategy):
@@ -6,3 +7,15 @@ class BbandsStrategy(Strategy):
         # Simply log the closing price of the series from the reference
         # logger.debug(f'{self.datas[0].datetime.datetime()}-close:{self.datas[0].close[0]}')
         pass
+
+
+class StFetcher(object):
+    _STRATS = [BbandsStrategy, BbandsStrategy]
+
+    def __new__(self, *args, **kwargs):
+        logger.info(f"**kwargs is {kwargs}")
+        idx = kwargs.pop('idx')
+        logger.info(f"idx is {idx},{len(self._STRATS)}")
+        obj = self._STRATS[idx](*args, **kwargs)
+        logger.info(f"obj is {obj}")
+        return obj
