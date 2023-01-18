7accd19bde3b2340300016d5c0d8ec54d11b9b38
chandlersong <chandler605@gmail.com>
2023-01-18 21:41:28 +0800

    add some solution for search example

--- basic/future_example.py
+++ basic/future_example.py
@@ -8,6 +8,10 @@ from backtrader.feeds import PandasData
 
 
 class TestFutureStrategy(bt.Strategy):
+    params = (('period', 15))
+
+    def __init__(self):
+        self.sma = bt.indicators.SMA(self.data, period=self.p.period)
 
     def next(self):
         date = bt.num2date(self.data.datetime[0])
@@ -28,14 +32,14 @@ class FutureCase(unittest.TestCase):
         broker = cerebro.broker
         broker.setcash(100.0)
         cerebro.adddata(compose_test_data(price))
-        cerebro.addstrategy(TestFutureStrategy)
+        cerebro.addstrategy(TestFutureStrategy, period=[20, 25])
         cerebro.run()
         logger.info('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
 
 def compose_test_data(price) -> PandasData:
     start = datetime.datetime(2023, 12, 1)
-    for idx,p in enumerate(price):
+    for idx, p in enumerate(price):
         p.insert(0, start + datetime.timedelta(days=idx))
     print(price)
     return PandasData(dataname=pd.DataFrame(price, columns=['datetime', 'open', 'high', 'low', 'close', 'volume']),
