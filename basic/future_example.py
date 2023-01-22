9e099418cc6698076086d38f4d5c37d7fe17488c
chandlersong <chandler605@gmail.com>
2023-01-22 15:00:41 +0800

    make an example margin not change

--- basic/future_example.py
+++ basic/future_example.py
@@ -3,11 +3,44 @@ import unittest
 
 import pandas as pd
 import backtrader as bt
+from backtrader import CommInfoBase
 from loguru import logger
 from backtrader.feeds import PandasData
 
 
+class FixMarginComm(CommInfoBase):
+    params = (
+        ('stocklike', False),
+        ('commtype', CommInfoBase.COMM_PERC),
+        ('percabs', True),
+        ('leverage', 2),
+        ('automargin', 0.1),
+    )
+
+    def cashadjust(self, size, price, newprice):
+        return 0
+
+
 class TestFutureStrategy(bt.Strategy):
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+            return
+
+            # Check if an order has been completed
+            # Attention: broker could reject order if not enough cash
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                logger.info('BUY EXECUTED, %.2f' % order.executed.price)
+            elif order.issell():
+                margin = order.executed.margin
+                if margin is None:
+                    margin = -1
+                logger.info('SELL EXECUTED, %.2f,Margin is %.2f' % (order.executed.price, margin))
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            logger.info('Order Canceled/Margin/Rejected')
+
     def next(self):
         date = bt.num2date(self.data.datetime[0])
         logger.info(f'{date},当前可用资金 {self.broker.getcash()},close is {self.data[0]}')
@@ -15,7 +48,7 @@ class TestFutureStrategy(bt.Strategy):
         logger.info(f'{date} 当前持仓量 {self.broker.getposition(self.data).size}')
 
         if date.day == 2:
-            self.sell(size=2)
+            self.sell(size=1)
 
         # if date.day == 5:
         #     self.close()
@@ -30,12 +63,7 @@ class FutureCase(unittest.TestCase):
         cerebro = bt.Cerebro()
         broker = cerebro.broker
         broker.setcash(100.0)
-        broker.setcommission(
-            commtype=bt.CommInfoBase.COMM_PERC,
-            mult=3.0,
-            stocklike=False,
-            automargin=0.1
-        )
+        broker.addcommissioninfo(FixMarginComm())
         cerebro.adddata(compose_test_data(price))
         cerebro.addstrategy(TestFutureStrategy)
         cerebro.run()
