a676e455cc10da910a11ccb0726397156623c4ee
chandler <chandler605@gmail.com>
2023-02-23 11:13:16 +0800

    finish close out prices calculate

--- basic/future.py
+++ basic/future.py
@@ -1,11 +1,12 @@
-
 class CalculateCloseOut:
 
     def __init__(self):
-        self.__min_margin_ratio = 0.005
+        self._min_margin_ratio = 0.005
         self._c_rate = 0.0001
 
-    def __call__(self, *args, **kwargs):
-        temp = self._c_rate + self.__min_margin_ratio
-
-        pass
+    def __call__(self, cash, position, open_price, is_long):
+        direction = -1
+        if is_long:
+            direction = 1
+        return (cash - position * open_price * direction) / (
+                position * (self._min_margin_ratio + self._c_rate - direction))
