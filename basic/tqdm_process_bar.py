4d1f46b6599121e4979fe669ec1582895ad2c087
chandlersong <chandler605@gmail.com>
2023-03-04 15:06:04 +0800

    add a tqdm example

--- basic/tqdm_process_bar.py
+++ basic/tqdm_process_bar.py
@@ -3,9 +3,14 @@ from time import sleep
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    pbar = tqdm(total=100)
+    outer = tqdm(total=100)
+    inter = tqdm(total=100)
     for i in range(10):
-        sleep(0.5)
-        pbar.update(10)
-        pbar.set_description(str(i))
-    pbar.close()
+
+        outer.update(10)
+        outer.set_description(str(i))
+        for j in range(10):
+            inter.update(10)
+            sleep(0.5)
+        inter.reset(100)
+    outer.close()
