fba9786b1a13310b7c28418fc9e0cfa1397a7878
chandlersong <chandler605@gmail.com>
2023-07-23 22:59:27 +0800

    update requirements

--- common/gruopby_demos.py
+++ common/gruopby_demos.py
@@ -36,6 +36,19 @@ class GroupByCase(unittest.TestCase):
         print(groups)
         self.assertTrue(True)
 
+    def test_find_max(self):
+        data = pd.read_csv("data1.csv", parse_dates=['candle_begin_time'], index_col=['candle_begin_time'])
+        del data["AtrVolatility_[3, 9, 0.3]"]
+        data.columns = ["symbol", "v1", "v2"]
+        x = data[data["v2"] > 0.5]
+        x.to_csv("middle.csv")
+        x = x.groupby("candle_begin_time").agg({'symbol': 'first', 'v1': 'max'})
+        x["tag"] = 1
+        print(x)
+        new_df = pd.merge(data, x, how='left', left_on=["candle_begin_time", 'symbol'],
+                          right_on=["candle_begin_time", 'symbol'], suffixes=('', '_DROP')).filter(regex='^(?!.*_DROP)')
+        print(new_df.to_csv("bb.csv"))
+
     def _group_info(self, g):
         return g["flag"].iloc[0] == "a"
 
